747164c215e0173dcc05e2d5d6a83a407d5404a5
Simon Funke <news-funsi@gmx.de>
2011-09-24 17:31:58 +0100

    login bar

--- social_addressbook/config/routes.rb
+++ social_addressbook/config/routes.rb
@@ -54,7 +54,7 @@ SocialAddressbook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "welcomes#index"
 
   # See how all your routes lay out with "rake routes"
 
