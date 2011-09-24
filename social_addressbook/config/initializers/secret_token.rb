d13457c5405e6092399ce7972c52c9c442ab7f96
Simon Funke <news-funsi@gmx.de>
2011-09-24 12:11:09 +0100

    initial commit

---
+++ social_addressbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialAddressbook::Application.config.secret_token = '98d88956ef7b91c25b701f221fec02b62c28f2f0f7a9e7014bacba7e3a3def3255b4c87de5babc003ff6209f5b418f0041bb2e98fceb4a39f01396543ed68d69'
