c9cd09cda55c38ad8957bf9491b87198a76af776
Simon Funke <news-funsi@gmx.de>
2011-09-25 13:28:00 +0100

    email verification

--- social_addressbook/config/environment.rb
+++ social_addressbook/config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SocialAddressbook::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+   :address => "mail.currentdomain.co.uk",
+   :authentication => :login,
+   :port => 26,
+   :domain => "incliqu.es",
+   :authentication => :login,
+   :user_name => "test@currentdomain.co.uk",
+   :password => "12345"
+}
