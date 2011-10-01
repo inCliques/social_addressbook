d760add255d84959e17c3daaa16abf168b284cad
Simon Funke <news-funsi@gmx.de>
2011-10-01 04:27:05 +0100

    fix invite mail

--- social_addressbook/app/mailers/invite_mailer.rb
+++ social_addressbook/app/mailers/invite_mailer.rb
@@ -4,13 +4,12 @@ class InviteMailer < ActionMailer::Base
   def send_invitation(inviter, invitee, group)
     @link = new_user_registration_url+'?email='+invitee.email
     @inviter = inviter
+    name_data_type_id = DataType.first(:conditions => { :name => 'Name' }).id
+    @inviter_name = @inviter.user_data.first(:all, :conditions => {:data_type_id => name_data_type_id}).value
     @invitee = invitee
     @group = group
-    if inviter.name.nil?
-      subject = "You got invited to join the "+group.name+" clique"
-    else
-      subject = inviter.name+" invites you to join the "+group.name+" clique"
-    end
+
+    subject = @inviter_name+" invites you to join the "+group.name+" clique"
     mail(:to => invitee.email, :subject => subject)  
   end  
 end
