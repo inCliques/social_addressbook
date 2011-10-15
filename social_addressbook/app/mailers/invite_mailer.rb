ac12a98c1f6f7c25b74cbc6e737c8c2f55daccec
Simon Funke <simon.funke@gmail.com>
2011-10-15 13:41:29 +0100

    Fix most bugs related to the recent data base change (the [offline]_user_data_type tables)

--- social_addressbook/app/mailers/invite_mailer.rb
+++ social_addressbook/app/mailers/invite_mailer.rb
@@ -2,9 +2,7 @@ class InviteMailer < ActionMailer::Base
   default :from => "no-reply@incliqu.es"
 
   def send_invitation(inviter, invitee, group)
-    require 'ruby-debug'
-    debugger
-    invitee_mail = invitee.get_datum_of_type('Email').first.value
+    invitee_email = invitee.find_datum_of_type('Email').first.value
     @link = new_user_registration_url+'?email='+invitee_email
     @inviter = inviter
     @invitee = invitee
