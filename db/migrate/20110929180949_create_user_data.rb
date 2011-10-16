6506dab08572ecc12b1353373462a72c60b250c8
Simon Funke <news-funsi@gmx.de>
2011-10-16 22:10:09 +0100

    set new user_data by default to "not verified"

--- db/migrate/20110929180949_create_user_data.rb
+++ db/migrate/20110929180949_create_user_data.rb
@@ -5,7 +5,7 @@ class CreateUserData < ActiveRecord::Migration
       t.integer :data_type_id # The type of this data. This is mainly used to sort the user data by types.
       t.string :name # A custom name for this data. While type.name might be "Phone", user_data.name might be more specific and chosen by the user e.g. "Work phone". 
       t.string :value # The value of the type, e.g. "+49(0)176/20032645"
-      t.boolean :verified # True if the user data is verified. 
+      t.boolean :verified, :default => false # True if the user data is verified. 
 
       t.timestamps
     end
