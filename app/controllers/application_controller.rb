6d046ff09d315945a5310348d691805bc22d3c6a
Simon Funke <news-funsi@gmx.de>
2011-10-16 21:24:17 +0100

    remove debugger statements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,6 @@ class ApplicationController < ActionController::Base
 
   # Send users to the edit user data when login in the first time
   def after_sign_in_path_for(resource_or_scope)
-    require 'ruby-debug'
-    debugger
     if resource_or_scope.is_a?(User) && resource_or_scope.sign_in_count<=1
       user_data_path 
     else
